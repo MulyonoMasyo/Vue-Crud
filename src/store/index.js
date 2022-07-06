e57a0014ab6490d82e76fd22a219cbbe46f41137
Mulyono Masyo <mulyono.masyo@gmail.com>
2022-07-06 10:55:18 +0700

    Belajar vue crud pertama

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    say: "hello world",
+    nama: "",
+    email: "",
+    updateNama: "",
+    updateEmail: "",
+    findEmail: "",
+    findNama: "",
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
